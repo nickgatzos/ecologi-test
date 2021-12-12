d694e2c805c9f28db4f29b6efeaabeed675a0b35
Nick Gatzoulis <ng17367@essex.ac.uk>
2021-12-12 23:53:52 +0000

    Implemented Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+// Core & MUI
+import React, { useState } from 'react';
+import { Box, AppBar, Toolbar, IconButton, Typography, CssBaseline } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+// Components
+import Sidebar from './components/Sidebar/Sidebar';
+
+// Styling
+import './App.scss';
+
+const drawerWidth = 208;
 
 const App = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
     <div className="App">
-      app
+      <CssBaseline />
+      <Box sx={{display: 'flex'}}>
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Sidebar drawerWidth={drawerWidth} mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,67 @@
+import { Toolbar, Divider, List, ListItem, Box, Drawer } from '@mui/material';
+import './Sidebar.scss';
+
+const Sidebar = ({mobileOpen, drawerWidth, handleDrawerToggle}) => {
+  const drawer = (
+    <div className="sidebar-content">
+      <Toolbar>
+        <a href="//ecologi.com">
+          <img src="https://ecologi.com/images/app/nav/logo.svg" alt="Ecologi Logo"/>
+        </a>
+      </Toolbar>
+      <Divider/>
+      <List>
+        <ListItem button selected>Dashboard</ListItem>
+        <ListItem button>Project Insights</ListItem>
+        <ListItem button>Badges</ListItem>
+      </List>
+      <Divider/>
+      <List>
+        <ListItem button>Invest in Ecologi</ListItem>
+        <ListItem button>FAQs</ListItem>
+        <ListItem button>Community Forum</ListItem>
+      </List>
+      <Divider/>
+      <List>
+        <ListItem button>Logout</ListItem>
+      </List>
+    </div>
+  );
+
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      aria-label="navigation"
+    >
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        className="sidebar"
+      >
+        {drawer}
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        className="sidebar"
+        open
+      >
+        {drawer}
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Sidebar;
